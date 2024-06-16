88e0a2c74c68ed69631483283b7b0e03cba06343
Jhonny <101852522+jhonnyfailed@users.noreply.github.com>
2024-06-16 09:32:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games = [2923300, 2784840, 3015610, 2977660];  // Enter here AppIDs of the needed games
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
